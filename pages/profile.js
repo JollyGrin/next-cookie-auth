761480e6190868ee24a82d71216814c24737e2d5
jollygrin <jollygrin@protonmail.com>
2019-07-31 13:46:27 +0200

    hello world

---
+++ pages/profile.js
@@ -0,0 +1,20 @@
+import Layout from "../components/Layout";
+import { getUserProfile } from "../lib/auth";
+
+export default class Profile extends React.Component {
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    getUserProfile().then(user => this.setState({ user }));
+  }
+
+  render() {
+    return (
+      <Layout title="Profile">
+        <pre>{JSON.stringify(this.state.user, null, 2)}</pre>
+      </Layout>
+    );
+  }
+}
